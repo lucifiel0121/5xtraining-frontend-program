e35c5b6009dfcd9b4cf5af06e02a2b6b0ee3667c
lucifiel0121 <zyi3204@gmail.com>
2020-02-17 12:26:38 +0800

    修改檔案結構

--- src/components/main/static-data.js
+++ src/components/main/static-data.js
@@ -1,32 +1,32 @@
 const json = {
   data: {
     bannerImgs: [
-      '/assets/images/banner_astro.jpg',
-      '/assets/images/banner_imcodingit.jpg',
-      '/assets/images/banner_mokumokukai.jpg',
-      '/assets/images/banner_5xruby-large.jpg',
+      '/assets/images/banner/banner_astro.jpg',
+      '/assets/images/banner/banner_imcodingit.jpg',
+      '/assets/images/banner/banner_mokumokukai.jpg',
+      '/assets/images/banner/banner_5xruby-large.jpg',
     ],
     featureLists: [
       {
-        featureImg: '/assets/images/feature-list-img1.png',
+        featureImg: '/assets/images/functionLists/feature-list-img1.png',
         featureTitle: ['網頁設計', '前後端課程教學'],
         featureContent:
           '帶你掌握 程式技能轉職工程師， 程式課程 從入門到進階讓你快速上手，學員好評推薦 轉職課程！',
       },
       {
-        featureImg: '/assets/images/feature-list-img2.png',
+        featureImg: '/assets/images/functionLists/feature-list-img2.png',
         featureTitle: ['頂尖技術', '網頁製作、專案開發'],
         featureContent:
           '網站開發到行動裝置 App 工程服務、系統設計開發甚至系統架設，提供專業的建議與頂尖的技術。',
       },
       {
-        featureImg: '/assets/images/feature-list-img3.png',
+        featureImg: '/assets/images/functionLists/feature-list-img3.png',
         featureTitle: ['資訊軟體開發', '技術顧問'],
         featureContent:
           '協助您將現有人力快速打造為 Ruby/Rails 團隊，無須經歷繁瑣的人才招募就有即戰力！',
       },
       {
-        featureImg: '/assets/images/feature-list-img4.png',
+        featureImg: '/assets/images/functionLists/feature-list-img4.png',
         featureTitle: ['前後端工程師', '社群經營'],
         featureContent:
           'Ruby 社群經營、舉辦各類活動，促進 Ruby 社群發展，歡迎前後端工程師分享交流。',
@@ -56,6 +56,50 @@ const json = {
         lectureTitle: 'Vue.js 與 Vuex 前端開發實戰課程 - 假日班',
         lectureTeacher: '許國政 (Kuro Hsu)',
         lectureTime: '3 月｜假日班',
+      },
+    ],
+    recommands: [
+      {
+        avatar: '/assets/images/avatar/victor.jpg',
+        name: 'Victor Lam',
+        jobTitle: '連續創業家，自由工作者',
+        drecommandDescription:
+          'Programmer 最有價值的是經驗值，「五倍紅寶石」的導師都是社群內數一數二的資深 Rubyist，由他們來帶領學員一探 Ruby 世界的神奇之處，實在是最佳選擇。',
+      },
+      {
+        avatar: '/assets/images/avatar/jason.jpg',
+        name: 'Jason Hs',
+        jobTitle: 'founder of The Big Questions & TEDxTaipei',
+        drecommandDescription:
+          'Eddie and his team are of the most dedicated Ruby evangelists I have ever come across. With their passion and professionalism, Ruby learners will leapfrog in their progress.',
+      },
+      {
+        avatar: '/assets/images/avatar/felix.jpg',
+        name: 'Felix Lin',
+        jobTitle: 'dappei.com 「搭配」網站創辦人',
+        drecommandDescription:
+          '還在尋找專業的 Ruby/Rails 師資嗎？別懷疑了，台灣最頂尖的 Rubyist 盡在「五倍紅寶石」！',
+      },
+      {
+        avatar: '/assets/images/avatar/hana.jpg',
+        name: 'Hana Chang',
+        jobTitle: 'Co-Founder and CEO of BountyHunter',
+        drecommandDescription:
+          '慕凡和龍哥是業界數一數二的 Ruby 大師，他們不光是底子深厚，也很懂得引導、訓練學習路上遇到挫折的 Ruby/Rails 學子。很慶幸有他們的協助讓我進入紅寶石的美妙世界。',
+      },
+      {
+        avatar: '/assets/images/avatar/johnsie.jpg',
+        name: 'John Sie',
+        jobTitle: 'Accuvally Inc.共同創辦人暨營運長',
+        drecommandDescription:
+          '選擇程式語言很重要，但選擇優秀的導師更重要！能跟擁有多年 Ruby/Rails 實戰經驗、活躍於社群的頂尖 Rubyist 學習，將一窺程式開發殿堂之藝術！我堅信：專業事讓專業的來，而專業的「五倍紅寶石」是寶藏，就等大家來挖掘。',
+      },
+      {
+        avatar: '/assets/images/avatar/benlin.jpg',
+        name: 'Ben Li',
+        jobTitle: '連續創業家，自由工作者',
+        drecommandDescription:
+          '「五倍紅寶石」的導師們都是業界經驗豐富的實戰高手，提供的課程及諮詢絕對能讓你大幅省去自行摸索的時間。',
       },
     ],
     showcaseInfo: [
