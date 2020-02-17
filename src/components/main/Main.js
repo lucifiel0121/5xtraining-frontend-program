e463683089b8f838ab18b8fdd3c6b19de120bceb
lucifiel0121 <zyi3204@gmail.com>
2020-02-17 12:26:45 +0800

    add 課程推薦

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import Test from './test/Test';
 import CarouselImg from './carousel-img/Carousel-img';
 import './main.scss';
 import FeatureSection from './feature-section/Feature-section';
 import Lecture from './lecture/Lecture';
 import json from './static-data';
 import Showcase from './showcase/Showcase';
+import Recommend from './recommend/Recommend';
 export default class Main extends Component {
   render() {
-    const { bannerImgs, featureLists, lectureInfo, showcaseInfo, socialIcon } = json.data;
+    const {
+      bannerImgs,
+      featureLists,
+      lectureInfo,
+      showcaseInfo,
+      socialIcon,
+      recommands,
+    } = json.data;
     return (
       <div className="main-container">
         <CarouselImg imgsSrc={bannerImgs} />
         <FeatureSection featureDescription={featureLists} />
         <Lecture title="熱門網頁設計課程推薦" lectureInfo={lectureInfo} />
-        <CarouselImg imgsSrc={bannerImgs} />
+        <Recommend title="網頁課程推薦" recommands={recommands} />
         <Showcase title="案例作品 Showcase" showcaseInfo={showcaseInfo} socialIcon={socialIcon} />
 
         <div className="scroll-to-top">
---
+++ src/components/main/recommend/Recommend.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './recommend.scss';
+import SectionTitle from '../section-title/Section-title';
+
+export default class Recommend extends Component {
+  state = {
+    actived: 0,
+  };
+  handleIndexClick = event => {
+    this.setState({
+      actived: Number(event.target.dataset.index),
+    });
+  };
+  render() {
+    const { actived } = this.state;
+    const { recommands, title } = this.props;
+    return (
+      <div className="recommend-container">
+        <SectionTitle title={title} />
+        {recommands.map(({ avatar, drecommandDescription, name, jobTitle }, index) => (
+          <div
+            className={
+              index === actived
+                ? 'carousel-recommend'
+                : 'carousel-recommend carousel-recommend-actived'
+            }
+            key={index}
+          >
+            <div className="carousel-recommend__avatar">
+              <img src={avatar} alt="avatar" />
+            </div>
+            <div className="carousel-recommend__content">
+              <p>{drecommandDescription}</p>
+              <span>{name}</span>
+              <span>{jobTitle}</span>
+            </div>
+          </div>
+        ))}
+
+        <div className="recommend-slider">
+          {recommands.map((x, index) => (
+            <div
+              className={
+                index === actived
+                  ? 'recommend-slider__li recommend-slider__li_actived'
+                  : 'recommend-slider__li'
+              }
+              data-index={index}
+              onClick={this.handleIndexClick}
+            ></div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
