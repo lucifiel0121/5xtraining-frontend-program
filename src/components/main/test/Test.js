c2306c6b030295cbd4192cfa91fd814a72eb861c
lucifiel0121 <zyi3204@gmail.com>
2020-02-18 11:49:10 +0800

    步驟5: 使用 React 改寫 五倍官網首頁

--- src/components/main/test/Test.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import './test.scss';
-
-export default class Test extends Component {
-  render() {
-    return <div className="testing-container">123</div>;
-  }
-}
