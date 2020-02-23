d4a103d303904abd98805972efaa52aab960b1ff
lucifiel0121 <zyi3204@gmail.com>
2020-02-23 22:38:04 +0800

    fix typo

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Header from './header/Header';
-import Footer from './footer/Footer';
-import Landing from './Landing';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import Header from './header/Header'
+import Footer from './footer/Footer'
+import Landing from './Landing'
+import { BrowserRouter as Router } from 'react-router-dom'
 export default class App extends React.Component {
-  handleClick = () => {
-    window.scrollTo(0, 0);
-  };
-  render() {
-    return (
-      <Router>
-        <Header />
-        <Landing />
-        <Footer />
-        <div className="scroll-to-top" onClick={this.handleClick}>
-          <i className="fa fa-angle-up"></i>
-        </div>
-      </Router>
-    );
-  }
+   handleClick = () => {
+      window.scrollTo(0, 0)
+   }
+   render() {
+      return (
+         <Router>
+            <Header />
+            <Landing />
+            <Footer />
+            <div className="scroll-to-top" onClick={this.handleClick}>
+               <i className="fa fa-angle-up"></i>
+            </div>
+         </Router>
+      )
+   }
 }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,75 +1,78 @@
-import React, { Component } from 'react';
-import './header.scss';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react'
+import './header.scss'
+import { NavLink } from 'react-router-dom'
 
 export default class Header extends Component {
-  state = {
-    navToggle: false,
-  };
+   state = {
+      navToggle: false,
+   }
 
-  handleNavClick = () => {
-    this.setState(currentState => {
-      return { navToggle: !currentState.navToggle };
-    });
-  };
+   handleNavClick = () => {
+      this.setState(currentState => {
+         return { navToggle: !currentState.navToggle }
+      })
+   }
 
-  render() {
-    const { navToggle } = this.state;
-    return (
-      <header className={navToggle ? 'navbar-actived' : ''}>
-        <div className="logo-link">
-          <NavLink to="/">
-            <img src={`${process.env.PUBLIC_URL}/assets/images/logo.png`} alt="logo" />
-          </NavLink>
-        </div>
+   render() {
+      const { navToggle } = this.state
+      return (
+         <header className={navToggle ? 'navbar-actived' : ''}>
+            <div className="logo-link">
+               <NavLink to="/">
+                  <img
+                     src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
+                     alt="logo"
+                  />
+               </NavLink>
+            </div>
 
-        <div className="navbar">
-          <NavLink to="/class" activeClassName="navbar__items_selected">
-            <li className="navbar__items">
-              <i className="far fa-gem"></i>
-              <p>線上課程</p>
-            </li>
-          </NavLink>
-          <NavLink to="/camp" activeClassName="navbar__items_selected">
-            <li className="navbar__items navbar__items_fake-tag">
-              <i className="far fa-compass fa-lg"></i>
-              <p>ASTRO Camp</p>
-            </li>
-          </NavLink>
-          <NavLink to="/talks" activeClassName="navbar__items_selected">
-            <li className="navbar__items">
-              <p>短期課程</p>
-            </li>
-          </NavLink>
-          <NavLink to="/project" activeClassName="navbar__items_selected">
-            <li className="navbar__items">
-              <p>專案開發</p>
-            </li>
-          </NavLink>
-          <NavLink to="/training" activeClassName="navbar__items_selected">
-            {' '}
-            <li className="navbar__items">
-              <p>企業代訓</p>
-            </li>
-          </NavLink>
-          <NavLink to="/space" activeClassName="navbar__items_selected">
-            {' '}
-            <li className="navbar__items">
-              <p>空間租借</p>
-            </li>
-          </NavLink>
-          <NavLink to="/posts" activeClassName="navbar__items_selected">
-            <li className="navbar__items">
-              <p>最新消息</p>
-            </li>
-          </NavLink>
-        </div>
-        <button className="nav-toggler" onClick={this.handleNavClick}>
-          <i className="icon-bar"></i>
-          <i className="icon-bar"></i>
-          <i className="icon-bar"></i>
-        </button>
-      </header>
-    );
-  }
+            <div className="navbar">
+               <NavLink to="/class" activeClassName="navbar__items_selected">
+                  <li className="navbar__items">
+                     <i className="far fa-gem"></i>
+                     <p>線上課程</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/camp" activeClassName="navbar__items_selected">
+                  <li className="navbar__items navbar__items_fake-tag">
+                     <i className="far fa-compass fa-lg"></i>
+                     <p>ASTRO Camp</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/talks" activeClassName="navbar__items_selected">
+                  <li className="navbar__items">
+                     <p>短期課程</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/project" activeClassName="navbar__items_selected">
+                  <li className="navbar__items">
+                     <p>專案開發</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/training" activeClassName="navbar__items_selected">
+                  {' '}
+                  <li className="navbar__items">
+                     <p>企業代訓</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/space" activeClassName="navbar__items_selected">
+                  {' '}
+                  <li className="navbar__items">
+                     <p>空間租借</p>
+                  </li>
+               </NavLink>
+               <NavLink to="/posts" activeClassName="navbar__items_selected">
+                  <li className="navbar__items">
+                     <p>最新消息</p>
+                  </li>
+               </NavLink>
+            </div>
+            <button className="nav-toggler" onClick={this.handleNavClick}>
+               <i className="icon-bar"></i>
+               <i className="icon-bar"></i>
+               <i className="icon-bar"></i>
+            </button>
+         </header>
+      )
+   }
 }
